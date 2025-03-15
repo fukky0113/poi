2118c9f9cea98191fc1502b184e551436aa497a8
fukky0113 <fukiyama0113@gmail.com>
2025-03-15 16:59:10 +0900

    追加 index, edit, update, destory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include SessionsHelper
+
+  private
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      redirect_to login_url, status: :see_other
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :show, :destroy] 
+  
   def new
     @user = User.new
   end
@@ -15,9 +17,29 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, status: :see_other
+    else 
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, status: :see_other
   end
 
   private 
