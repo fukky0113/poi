24bfd9073748c863767d69cba18897fe8e530f72
fukky0113 <fukiyama0113@gmail.com>
2025-03-19 10:33:32 +0900

    追加 基本的なランキング機能を実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,11 @@ class StaticPagesController < ApplicationController
   def home
     @post = Post.all.order(created_at: "DESC")
     gon.points = @post.pluck(:latitude, :longitude)
+
     @user_ranking = Post.group(:user_id).order('count(user_id) DESC').limit(3).pluck(:user_id)
+    @area_ranking = Post.group(:point).order('count(point) DESC').limit(3).pluck(:point)
+    @fav_ranking = Favorite.group(:post_id).order('count(post_id) DESC').limit(3).pluck(:post_id)
+
     @user = User.all
   end
 end
