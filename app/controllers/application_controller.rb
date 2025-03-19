f2710ad235a21171cd9a22dd406db4be1e3390f6
fukky0113 <fukiyama0113@gmail.com>
2025-03-19 15:37:32 +0900

    追加 ヒーロエリア

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,34 @@ class ApplicationController < ActionController::Base
       redirect_to login_url, status: :see_other
     end
   end
+
+  def post_point_count(area_ranking)
+    rank = []
+    total = 0
+    area_ranking.each do |area_rank|
+      count = Post.where(point: area_rank).count
+      rank.push(count)
+      total += count
+    end
+    rank.push(Post.count-total)
+    return rank
+  end
+
+  def post_category_count(category_chart)
+    rank = []
+    category_chart.each do |category|
+      count = Post.where(category_id: category).count
+      rank.push(count)
+    end
+    return rank
+  end
+
+  def id2name(category_chart)
+    ret = []
+    category_chart.each do |category|
+      name = Category.find(category).g_type
+      ret.push(name)
+    end
+    return ret
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,17 @@ class StaticPagesController < ApplicationController
   def home
     @post = Post.all.order(created_at: "DESC")
     gon.points = @post.pluck(:latitude, :longitude)
+    
 
     @user_ranking = Post.group(:user_id).order('count(user_id) DESC').limit(3).pluck(:user_id)
-    @area_ranking = Post.group(:point).order('count(point) DESC').limit(3).pluck(:point)
     @fav_ranking = Favorite.group(:post_id).order('count(post_id) DESC').limit(3).pluck(:post_id)
+    @area_ranking = Post.group(:point).order('count(point) DESC').pluck(:point)
+    gon.area_ranking = @area_ranking.slice(0,10)
+    gon.area_ranking_count = post_point_count(@area_ranking.slice(0,10))
+ 
+    category_chart = Post.group(:category_id).order('count(category_id) DESC').limit(3).pluck(:category_id)
+    gon.category_chart = id2name(category_chart)
+    gon.category_chart_count = post_category_count(category_chart)
 
     @user = User.all
   end
