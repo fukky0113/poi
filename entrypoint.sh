36b20948f7bc974abc4a3ffb40173346503bb698
fukky0113 <fukiyama0113@gmail.com>
2025-03-20 16:55:43 +0900

    修正 migrate重複エラー

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ rm -f /myapp/tmp/pids/server.pid
 mkdir -p /myapp/tmp/sockets
 
 # bundle exec rails db:create
-bundle exec rails db:migrate
+bundle exec rails db:migrate:reset
 bundle exec rails db:seed
 bundle exec rails assets:precompile
 
